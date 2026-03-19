15c9a15c41457825d068d841a4f86c612529bc65
Suhan Park <park94@kw.ac.kr>
2026-03-19 20:55:51 +0900

    Bump version from 0.1.0 to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='action_lipo',
-    version='0.1.0',
+    version='0.2.0',
     description='A lightweight post-optimizer for action chunks',
     author='Suhan Park',
     author_email='park94@kw.ac.kr',
@@ -10,6 +10,7 @@ setup(
     install_requires=[
         'numpy>=1.18.0',
         'cvxpy>=1.1.0',
+        'osqp>=1.0.0'
     ],
     python_requires='>=3.7',
     classifiers=[
